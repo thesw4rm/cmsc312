c83e025ae74cfc401183ab0ed11595263a4515e6
sw4rm <ytpillai@pm.me>
2020-04-14 17:10:49 -0400

    Updated constants.h metadata to work for mmap and extra credit portion
    Signed-off-by: sw4rm <ytpillai@pm.me>

--- constants.h
+++ constants.h
@@ -11,26 +11,22 @@
 #define DEFAULT_PPROCS 10
 #define PRINT_QUEUE_LIMIT 30
 
-
-
-
 // Printer format: 1, printer_id, user_id, bytes, start_time, wait_time
 // User format: 0, -1, user_id, bytes, start_time, 0
 
-#include "dll.h"
 #include "csema_bad.h"
+#include "dll.h"
 
 typedef struct {
-    unsigned int log: 1; // Should the program log verbose data or not
-    unsigned int demo: 1; // Should the program do printf statements
-    unsigned int free_space; // Each bit says that index is free for another print job
-    int user_procs_left; // Number of user processes left to execute (set directly to 0 in case of sigint)
-    int head; // Offset to get to the first item in print queue
-    pthread_mutex_t qmut; // Mutex for print queue
-    csema_t qempty; // Semaphore for when queue has no print jobs
-    csema_t qfull; // Semaphore for when print queue is full
-    csema_t uprocs_comp; // Post when all user processes are complete
-    csema_t pprocs_comp;
+  int user_procs_left; // Number of user processes left to execute (set directly
+                       // to 0 in case of sigint)
+  job *head;           // Offset to get to the first item in print queue
+  sem_t pqmut[30];     // Mutexes for print queue
+  sem_t qmut;          // Mutex for metadata
+  csema_t qempty;      // Semaphore for when queue has no print jobs
+  csema_t qfull;       // Semaphore for when print queue is full
+  csema_t uprocs_comp; // Post when all user processes are complete
+  csema_t pprocs_comp;
 
 } shm_mdata;
 
@@ -39,16 +35,13 @@ typedef struct {
 
 #define QUEUE_OFFSET sizeof(shm_mdata)
 
+#endif // A3_CONSTANTS_H
 
-#endif //A3_CONSTANTS_H
-
-
-/* if (mdata->qtail == NULL) { // Tail should not be NULL unless this is first print job (thread and i are 0)
+/* if (mdata->qtail == NULL) { // Tail should not be NULL unless this is first
+print job (thread and i are 0)
 //            if ((tn + i) != 0)
-//                printf("qtail was NULL when user %d %d was trying to add a new print job\n", tn, i);
-         if (mdata->qhead == NULL)
-             mdata->qhead = njob;
-         else {
+//                printf("qtail was NULL when user %d %d was trying to add a new
+print job\n", tn, i); if (mdata->qhead == NULL) mdata->qhead = njob; else {
              mdata->qtail = njob;
              mdata->qtail->prev = mdata->qhead;
              mdata->qhead->next = mdata->qtail;
