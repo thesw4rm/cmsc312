7e3c41e6ccd3fbb4225106fa3f8d6db67b6ba48d
sw4rm <ytpillai@pm.me>
2020-04-14 17:11:25 -0400

    Updated linkedlist to use pointers
    Signed-off-by: sw4rm <ytpillai@pm.me>

--- dll.c
+++ dll.c
@@ -8,21 +8,9 @@ void init_fcfs_job(job *njob, int bytes, unsigned int off) {
     njob->add_job = add_job_fcfs;
     njob->rm_job = rm_job;
     njob->bytes = bytes;
-    njob->next = -1;
-    njob->off = off;
+    njob->next = NULL;
 }
 
-/**
- *
- * @param njob - Creates new job list/queue from existing and allocated pointer
- */
-void init_sjf_job(job *njob, int bytes, unsigned int off) {
-    njob->add_job = add_job_sjf;
-    njob->rm_job = rm_job;
-    njob->bytes = bytes;
-    njob->off = off;
-    njob->next = -1;
-}
 
 /**
  * Adds job to end of list (First Come First Serve)
@@ -32,49 +20,24 @@ void init_sjf_job(job *njob, int bytes, unsigned int off) {
 
 struct print_job *add_job_fcfs(struct print_job *cjob, struct print_job *njob) {
     if(cjob == NULL){ // Adding to empty queue
-//        printf("WAT ITS NULL?\n");
         return njob;
     }
-    int hoff = cjob->off;
-//    printf("\n");
-//    printf("WE WERE CALLED? %u\n", cjob->next);
-    while (cjob->next != -1) {
-//        printf("NUMSTART %d\n", cjob[cjob->next - cjob->off].off);
-        cjob = &cjob[cjob->next - cjob->off];
-//        printf("%u %u\n", cjob->off, cjob->next);
+    struct print_job  *hoff = cjob;
+    while (cjob->next != NULL) {
+        cjob = cjob->next;
     }
 
-//    printf("ASDASD\n");
-    njob->next = cjob->next;
-    cjob->next = njob->off;
+    cjob->next = njob;
 
     //printf("%u %u\n", hoff - cjob->off, cjob->off);
 
-    return &cjob[hoff - cjob->off];
+    return hoff;
 
 }
 
-struct print_job *add_job_sjf(struct print_job *cjob, struct print_job *njob) {
-    if(njob->bytes < cjob->bytes){
-        njob->next = cjob->off;
-        return njob;
-    }
-    int hoff = cjob->off;
-    while (cjob->bytes <= njob->bytes && cjob->next != -1) {
-        cjob = &cjob[cjob->next - cjob->off]; // Negative array index means go backwards that many pointers
-    }
-    njob->next = cjob->next;
 
-    cjob->next = njob->off;
-    return &cjob[hoff - cjob->off];
-
-}
-
-unsigned int rm_job(struct print_job *rjob) {
-    if (rjob->next != -1)
-        return rjob->next;
-    else
-        return -1;
+struct print_job *rm_job(struct print_job *rjob) {
+    return rjob->next;
 }
 
 /**
--- dll.h
+++ dll.h
@@ -3,25 +3,24 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <time.h>
-typedef struct print_job{
-    int next; // Offset of next job
-    int off; // Offset of current job
-   unsigned int thd, bytes; // Thread that assigned the job, and the bytes allocated
-   clock_t start_wait_time; // Total time job has been waiting
+typedef struct print_job {
+    struct print_job *next;
+  unsigned int thd,
+      bytes, // Thread that assigned the job, and the bytes allocated
+      f; // 0 if slot is not free, 1 if slot is free
+  clock_t start_wait_time; // Total time job has been waiting
 
+  struct print_job *(*add_job)(struct print_job *, struct print_job *);
+  struct print_job *(*rm_job)(struct print_job *);
 
-   struct print_job *(*add_job)(struct print_job *, struct print_job *);
-   unsigned int (*rm_job)(struct print_job *);
 } job;
 
 struct print_job *add_job_sjf(struct print_job *, struct print_job *);
 struct print_job *add_job_fcfs(struct print_job *, struct print_job *);
 
-unsigned int rm_job(struct print_job *);
+struct print_job *rm_job(struct print_job *);
 
 void init_fcfs_job(job *, int, unsigned int);
 void init_sjf_job(job *, int, unsigned int);
-
-
 
 #endif
